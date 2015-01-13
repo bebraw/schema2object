d6e3ad2947d75dfd6ce5be13bea8be80f5394962
Juho Vepsalainen <bebraw@gmail.com>
2015-01-13 14:07:38 +0200

    Add a demo

---
+++ demo/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var schema2object = require('../');
+var definitions = require('./definitions');
+
+
+main();
+
+function main() {
+    Object.keys(definitions).forEach(function(name) {
+        var o = getObject(definitions, name);
+
+        if(o) {
+            console.log(name + ':\n', JSON.stringify(o, null, 4), '\n');
+        }
+    });
+}
+
+function getObject(definitions, name) {
+    var definition = definitions[name];
+    var properties = definition.properties;
+
+    if(!properties) {
+        return;
+    }
+
+    if(definition.required) {
+        properties = schema2object.getRequiredProperties(definition);
+    }
+
+    return schema2object.properties2object(definitions, properties);
+}
